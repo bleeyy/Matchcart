a725914a67ec9d92ff0c3bc38a187e1e26718215
bleeyy <benjaminle17@outlook.com>
2026-08-16 11:03:38 -0500

    removed temp icons

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <>
       <h1 className="text-4xl font-bold text-center mb-2">
-        🛒 MatchCart
+        MatchCart
       </h1>
 
       <p className="text-center text-gray-500 mb-8">
